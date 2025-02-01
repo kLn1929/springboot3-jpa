76f0597f63de0482998158dd3ca6469856dc7a01
Kelvin <guimaraes.kln@gmail.com>
2025-02-01 20:23:33 -0300

    Category

---
+++ src/main/java/com/kln/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.kln.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kln.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
